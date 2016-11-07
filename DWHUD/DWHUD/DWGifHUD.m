c5349e4a9b5ab5bdf9fffdf5ced557cf4542a613
Wicky <codeWicky@163.com>
2016-11-07 20:27:41 +0800

    添加DWGifHUD

---
+++ DWHUD/DWHUD/DWGifHUD.m
@@ -0,0 +1,21 @@
+//
+//  DWGifHUD.m
+//  DWHUD
+//
+//  Created by Wicky on 2016/11/7.
+//  Copyright © 2016年 Wicky. All rights reserved.
+//
+
+#import "DWGifHUD.h"
+
+@implementation DWGifHUD
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
